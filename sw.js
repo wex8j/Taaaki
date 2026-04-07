014b5cc3b82db07158277ddcfdfd6bfb36fcbcef
wex8j <gw97mm3@gmail.com>
2026-04-07 07:41:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,3 @@
-const CACHE_NAME = '3tx-v1';
-const urlsToCache = [
-  '/',
-  '/index.html'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', function(event) {
+  // تفعيل وضع الأوفلاين الأساسي
 });
